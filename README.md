6160a5d187a58bce1b325dfb1f34beac4bb8043a
c00k1ez <egorplotnikov18@gmail.com>
2026-05-16 15:46:10 +0200

    upd

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trained tokenizers
+Here you could find pre-trained tokenizers w/ different texts, different languages and different backends.
+## Available tokenizers
+| Name | Trained data | Vocab size | Language | Library |
+| -------- | -------- | -------- | ------- | ------- |
+| [en_yttm_tokenizer.model](trained_tokenizers/yttm/en_yttm_tokenizer.model) | N/A | N/A | eng | [yttm](https://github.com/vkcom/youtokentome) |
+| [ru_yttm_tokenizer.model](trained_tokenizers/yttm/ru_yttm_tokenizer.model) | N/A | N/A | ru | [yttm](https://github.com/vkcom/youtokentome) |
+| [en_tokenizers_wikitext_32000.json](trained_tokenizers/tokenizers/en_tokenizers_wikitext_32000.json) | [wikitext-103-raw-v1](https://huggingface.co/datasets/Salesforce/wikitext) | 32_000 | eng | [tokenizers](https://github.com/huggingface/tokenizers) |
---
+++ src/train_tokenizers.py
@@ -0,0 +1,43 @@
+import click
+import datasets
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+
+from pathlib import Path
+
+
+def dataset_iterator(dataset, batch_size=100):
+    # Only keep the text column to avoid decoding the rest of the columns unnecessarily
+    tok_dataset = dataset.select_columns("text")
+    for batch in tok_dataset.iter(batch_size):
+        yield batch["text"]
+
+
+@click.command()
+def main():
+    base_dir = Path("./trained_tokenizers")
+
+    pretrain_dataset = datasets.load_dataset(
+        "Salesforce/wikitext", "wikitext-103-raw-v1"
+    )
+    pretrain_dataset = pretrain_dataset.filter(lambda x: len(x["text"]) > 200)
+
+    tokenizer = Tokenizer(BPE())
+    trainer = BpeTrainer(
+        vocab_size=32_000, special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"]
+    )
+
+    tokenizer.train_from_iterator(
+        dataset_iterator(pretrain_dataset["train"]), trainer=trainer
+    )
+
+    tokenizers_dir = base_dir / "tokenizers"
+    if not tokenizers_dir.exists():
+        tokenizers_dir.mkdir()
+
+    tokenizer.save(str(tokenizers_dir / "en_tokenizers_wikitext_32000.json"))
+
+
+if __name__ == "__main__":
+    main()
